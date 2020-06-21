e823f07164c7fe7b5c1e45b4b271416fe6cd51f5
AncutaBiro <ancuta.biro@yahoo.com>
2020-06-21 22:00:56 +0300

    Initial commit (project structure)

---
+++ src/test/java/org/fasttrackit/movieexplorer/MovieExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fasttrackit.movieexplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieExplorerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
