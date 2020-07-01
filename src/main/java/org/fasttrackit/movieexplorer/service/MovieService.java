e576b6d365d24846ee691def97c143b4920cbb84
AncutaBiro <ancuta.biro@yahoo.com>
2020-07-01 19:33:33 +0300

    added logger in product service

--- src/main/java/org/fasttrackit/movieexplorer/service/MovieService.java
+++ src/main/java/org/fasttrackit/movieexplorer/service/MovieService.java
@@ -3,11 +3,15 @@ package org.fasttrackit.movieexplorer.service;
 import org.fasttrackit.movieexplorer.domain.Movie;
 import org.fasttrackit.movieexplorer.persistence.MovieRepository;
 import org.fasttrackit.movieexplorer.transfer.SaveMovieRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MovieService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MovieService.class);
 
     private final MovieRepository movieRepository;
 
@@ -17,7 +21,7 @@ public class MovieService {
     }
 
     public Movie createMovie (SaveMovieRequest request) {
-        System.out.println("Creating movie: " + request);
+        LOGGER.info("Creating product {}", request);
 
         Movie movie= new Movie();
         movie.setName(request.getName());
